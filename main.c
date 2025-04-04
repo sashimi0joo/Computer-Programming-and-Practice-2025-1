f083772329f215d75ebe44ad99ca28b6ed3ae4b1
wjddms18 <88753974+wjddms18@users.noreply.github.com>
2025-04-04 19:33:36 +0900

    change

--- main.c
+++ main.c
@@ -4,10 +4,19 @@ void add_pixel(int red, int green, int blue);
 void make_png(int width, int height);
 
 int main() {
+
+	add_pixel(0, 0, 0);
+	add_pixel(0, 0, 0);
+	add_pixel(0, 0, 0);
+
+	add_pixel(0, 0, 0);
 	add_pixel(255, 0, 0);
-	add_pixel(255, 0, 0);
-	add_pixel(0, 255, 0);
-	add_pixel(0, 255, 0);
-	add_pixel(0, 0, 255);
-	make_png(3, 2);
+	add_pixel(0, 0, 0);
+
+	add_pixel(0, 0, 0);
+	add_pixel(0, 0, 0);
+	add_pixel(0, 0, 0);
+
+	make_png(3, 3);
+
 }
